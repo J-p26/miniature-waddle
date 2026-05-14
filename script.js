d50f65be7d31ad0b02a8198a7270fab2304e44f4
J-p26 <j.wanford@pm.me>
2026-05-14 11:56:55 +0100

    Update ISS HDEV Stream 1 URL to new iframe format

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const issStreams = [
     {
         name: "ISS HDEV Stream 1",
-        url: "https://www.youtube.com/embed/21X5lGlDOfg"
+        url: "<iframe width="560" height="315" src="https://www.youtube.com/embed/FuuC4dpSQ1M?si=tTmRLJTa-w_19QOR&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"
     },
     {
         name: "ISS HDEV Stream 2",
